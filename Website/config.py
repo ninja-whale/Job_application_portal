92e2738e3d101c54e25a57064709bf63ac57bac9
Payal <payalmalviya9118@gmail.com>
2024-08-29 12:52:51 +0530

    Update config.py

--- Website/config.py
+++ Website/config.py
@@ -4,5 +4,5 @@ class Config:
     SECRET_KEY = os.urandom(24)
     SQLALCHEMY_DATABASE_URI = 'sqlite:///resume_upload.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = 'uploads'
+    UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
     ALLOWED_EXTENSIONS = {'pdf', 'doc', 'docx'}
